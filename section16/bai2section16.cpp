42057801249527bbea95cf5f5e0e519c04063331
vinhpham-cntt2-hk25 <dragonzoro17@gmail.com>
2025-11-12 22:52:40 +0700

    Add files via upload

--- section16/bai2section16.cpp
+++ section16/bai2section16.cpp
@@ -3,7 +3,7 @@
  
  int main(){
     char str[] = "Hello World";
-    printf("The declared string is :%s\n");
+    printf("The declared string is :%s\n",str);
     printf("The characters in the string are:");
     for (int i = 0; i < strlen(str); i++){
         printf("%c", str[i]);
